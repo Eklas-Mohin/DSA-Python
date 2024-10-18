18462d17aa9db891f3ea7fc630bade8b08673dde
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 17:05:29 +0600

    Basic Sorting Algorithm - Bubble Sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,17 +1,24 @@
-def main():
-    arr = [17, 23, 7, 59, 73, 13, 37, 53, 47]
-    print('Unsorted Array:', arr, '\n')
+def bubble_sort(arr):
+    n = len(arr)
     
-    for i in range(len(arr) - 1):
-        is_swapped = False
-        for j in range(len(arr) - i - 1):
+    print('Unsorted Array:', arr, '\n')
+
+    for i in range(n):
+        swapped = False
+        
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                is_swapped = True
-        if is_swapped == False:
+                swapped = True
+        
+        if not swapped:
             break
-                
-    print('Sorted Array:  ', arr)
     
-if __name__ == "__main__":
+    print('Sorted Array:  ', arr)
+
+def main():
+    arr = [17, 23, 7, 59, 73, 13, 37, 53, 47]
+    bubble_sort(arr)
+
+if __name__ == '__main__':
     main()
