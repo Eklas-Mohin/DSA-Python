683002ce95e803a0cb65cc9f1908b70084889c14
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 17:05:57 +0600

    Basic Sorting Algorithm - Selection Sort

--- selection_sort.py
+++ selection_sort.py
@@ -2,19 +2,19 @@ def selection_sort(arr):
     n = len(arr)
     
     print('Unsorted Array:', arr, '\n')
-
+    
     for i in range(n - 1):
         min_idx = i
         for j in range(i + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[min_idx], arr[i] = arr[i], arr[min_idx]
-
-    print('Sorted Array:  ', arr)
     
+    print('Sorted Array:  ', arr)
+
 def main():
     arr = [17, 23, 7, 59, 73, 13, 37, 53, 47]
     selection_sort(arr)
-    
+
 if __name__ == '__main__':
     main()
